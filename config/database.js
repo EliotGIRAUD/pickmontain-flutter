a8d659410b1c2721d19fbb0f5924a6032fa13266
eliot <eliotgiraud@gmail.com>
2025-04-18 13:23:07 +0200

    fix db connection

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_URI
-);
-
+const sequelize = new Sequelize(process.env.DB_URI, {
+  dialect: 'mysql',
+  logging: false,
+});
 // Test de connexion
 (async () => {
   try {
