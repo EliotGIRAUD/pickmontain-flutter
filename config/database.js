d7b7bc315adddb48e09d3830f7a07ed786d4bdb9
eliot <eliotgiraud@gmail.com>
2025-04-18 11:56:17 +0200

    simple autentification for db

--- config/database.js
+++ config/database.js
@@ -2,14 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false, // Désactive les logs SQL dans la console
-  }
+  process.env.DB_URI
 );
 
 // Test de connexion
